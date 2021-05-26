361aa0769e0c6d0357186ad84bfef9362c62175d
brtkuu <bartoszposert5@gmail.com>
2021-05-26 12:09:22 +0200

    apod change

--- src/APOD/apod.service.ts
+++ src/APOD/apod.service.ts
@@ -13,21 +13,19 @@ export class APODService {
 
   async updateAPOD() {
     await this.apodApi.deleteAPODS();
-    for (let i = 0; i < 7; i++) {
-      const date = new Date(new Date().getTime() - i * 86400000);
-      const stringDate = `${date.getFullYear()}-${date.getMonth() +
-        1}-${date.getDate()}`;
+    const date = new Date();
+    const stringDate = `${date.getFullYear()}-${date.getMonth() +
+      1}-${date.getDate()}`;
 
-      const data = await axios.get(
-        `https://api.nasa.gov/planetary/apod?api_key=HSQg2IZMuBBelkbTvL0FJHLtdCv6BwmfUsWPYoLy&date=${stringDate}`,
-      );
-      await this.apodApi.updateAPOD({
-        title: data.data.title,
-        url: data.data.url,
-        explination: data.data.explination,
-        date: data.data.date,
-        media: data.data.media_type,
-      });
-    }
+    const data = await axios.get(
+      `https://api.nasa.gov/planetary/apod?api_key=HSQg2IZMuBBelkbTvL0FJHLtdCv6BwmfUsWPYoLy&date=${stringDate}`,
+    );
+    await this.apodApi.updateAPOD({
+      title: data.data.title,
+      url: data.data.url,
+      explination: data.data.explination,
+      date: data.data.date,
+      media: data.data.media_type,
+    });
   }
 }
